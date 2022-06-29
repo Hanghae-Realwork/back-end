da2aad26d7dda06a293af227f3742c59626edf9b
engin9803 <mne9691@gmail.com>
2022-06-29 11:27:57 +0900

    Docs: models의 Date 타입 변경

--- models/resume.js
+++ models/resume.js
@@ -1,56 +1,59 @@
 const mongoose = require("mongoose");
 
-const ResumeSchema = new mongoose.Schema({
-  // resumeId: {
-  //   type: String,
-  //   required: true
-  // },
-  userId: {
-    type: String,
-    required: true,
+const ResumeSchema = new mongoose.Schema(
+  {
+    // resumeId: {
+    //   type: String,
+    //   required: true
+    // },
+    userId: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    content2: {
+      type: String,
+      required: true,
+    },
+    content3: {
+      type: String,
+      required: true,
+    },
+    start: {
+      type: Date,
+      required: true,
+    },
+    end: {
+      type: Date,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    skills: {
+      type: Array,
+      required: true,
+      default: [],
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
   },
-  title: {
-    type: String,
-    required: true,
-  },
-  content: {
-    type: String,
-    required: true,
-  },
-  content2: {
-    type: String,
-    required: true,
-  },
-  content3: {
-    type: String,
-    required: true,
-  },
-  start: {
-    type: String,
-    required: true,
-  },
-  end: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    required: true,
-  },
-  skills: {
-    type: Array,
-    required: true,
-    default: [],
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  phone: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 ResumeSchema.virtual("resumeId").get(function () {
   return this._id.toHexString();
 });
